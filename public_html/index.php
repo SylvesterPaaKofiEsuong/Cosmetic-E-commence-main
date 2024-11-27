4a2ff152c4a15826322961fdde105af3bb893baf
sylvesterpaakofiesuong <sygycalat@gmail>
2024-11-27 22:25:00 +0000

    nav

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" href="images/Favicon1.png" sizes="32x32">
 </head>
 <body>
-    <!-- Header -->
+    <!-- Header and Navigation -->
     <?php 
     include './includes/header.php'; 
     ?>
